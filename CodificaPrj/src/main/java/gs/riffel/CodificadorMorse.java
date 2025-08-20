71dd1deaf68b97b450b4b4275d5db96ea2b1bb06
GustaRiffel <g.riffel@edu.pucrs.br>
2025-08-20 15:34:40 -0300

    att

--- CodificaPrj/src/main/java/gs/riffel/CodificadorMorse.java
+++ CodificaPrj/src/main/java/gs/riffel/CodificadorMorse.java
@@ -13,72 +13,112 @@ public class CodificadorMorse implements Codificador {
     }
 
     public LocalDate getDataCriacao() {
-        return LocalDate.of(2025, 03, 12);
+        return LocalDate.of(2025, 3, 12);
     }
 
     public int getNivelSeguranca(){
         return 40;
     }
 
-
     static {
-        morseMap.put('A', ".-");
-        morseMap.put('B', "-...");
-        morseMap.put('C', "-.-.");
-        morseMap.put('D', "-..");
-        morseMap.put('E', ".");
-        morseMap.put('F', "..-.");
-        morseMap.put('G', "--.");
-        morseMap.put('H', "....");
-        morseMap.put('I', "..");
-        morseMap.put('J', ".---");
-        morseMap.put('K', "-.-");
-        morseMap.put('L', ".-..");
-        morseMap.put('M', "--");
-        morseMap.put('N', "-.");
-        morseMap.put('O', "---");
-        morseMap.put('P', ".--.");
-        morseMap.put('Q', "--.-");
-        morseMap.put('R', ".-.");
-        morseMap.put('S', "...");
-        morseMap.put('T', "-");
-        morseMap.put('U', "..-");
-        morseMap.put('V', "...-");
-        morseMap.put('W', ".--");
-        morseMap.put('X', "-..-");
-        morseMap.put('Y', "-.--");
-        morseMap.put('Z', "--..");
+        morseMap.put('A', ".-");   
+        morseMap.put('a', ".-");
+        morseMap.put('B', "-..."); 
+        morseMap.put('b', "-...");
+        morseMap.put('C', "-.-."); 
+        morseMap.put('c', "-.-.");
+        morseMap.put('D', "-..");  
+        morseMap.put('d', "-..");
+        morseMap.put('E', ".");    
+        morseMap.put('e', ".");
+        morseMap.put('F', "..-."); 
+        morseMap.put('f', "..-.");
+        morseMap.put('G', "--.");  
+        morseMap.put('g', "--.");
+        morseMap.put('H', "...."); 
+        morseMap.put('h', "....");
+        morseMap.put('I', "..");   
+        morseMap.put('i', "..");
+        morseMap.put('J', ".---"); 
+        morseMap.put('j', ".---");
+        morseMap.put('K', "-.-");  
+        morseMap.put('k', "-.-");
+        morseMap.put('L', ".-.."); 
+        morseMap.put('l', ".-..");
+        morseMap.put('M', "--");   
+        morseMap.put('m', "--");
+        morseMap.put('N', "-.");   
+        morseMap.put('n', "-.");
+        morseMap.put('O', "---");  
+        morseMap.put('o', "---");
+        morseMap.put('P', ".--."); 
+        morseMap.put('p', ".--.");
+        morseMap.put('Q', "--.-"); 
+        morseMap.put('q', "--.-");
+        morseMap.put('R', ".-."); 
+        morseMap.put('r', ".-.");
+        morseMap.put('S', "..."); 
+        morseMap.put('s', "...");
+        morseMap.put('T', "-");    
+        morseMap.put('t', "-");
+        morseMap.put('U', "..-"); 
+        morseMap.put('u', "..-");
+        morseMap.put('V', "...-"); 
+        morseMap.put('v', "...-");
+        morseMap.put('W', ".--");  
+        morseMap.put('w', ".--");
+        morseMap.put('X', "-..-"); 
+        morseMap.put('x', "-..-");
+        morseMap.put('Y', "-.--"); 
+        morseMap.put('y', "-.--");
+        morseMap.put('Z', "--.."); 
+        morseMap.put('z', "--..");
         morseMap.put(' ', "/");
+
+        morseMap.put('!', "-.-.--");
+        morseMap.put('.', ".-.-.-");
+        morseMap.put(',', "--..--");
+        morseMap.put('?', "..--..");
 
         // Preenche o mapa reverso
         for (Map.Entry<Character, String> entry : morseMap.entrySet()) {
             reverseMorseMap.put(entry.getValue(), entry.getKey());
         }
     }
-    
 
     public String codificar(String texto) {
-        StringBuilder morse = new StringBuilder();
-        texto = texto.toUpperCase();
-        for (char c : texto.toCharArray()) {
-            String code = morseMap.get(c);
+    StringBuilder morse = new StringBuilder();
+
+    for (char caracter : texto.toCharArray()) {
+        if (Character.isLetter(caracter)) {
+            char maiuscula = Character.toUpperCase(caracter);
+            String code = morseMap.get(maiuscula);
+            if (code != null) {
+                morse.append(code).append(" ");
+            }
+        } else {
+            String code = morseMap.get(caracter);
             if (code != null) {
                 morse.append(code).append(" ");
             }
         }
-        return morse.toString().trim();
     }
-
-    public String decodificar(String morse) {
-        StringBuilder texto = new StringBuilder();
-        String[] codes = morse.split(" ");
-        for (String code : codes) {
-            Character c = reverseMorseMap.get(code);
-            if (c != null) {
-                texto.append(c);
-            }
-        }
-        return texto.toString();
-    }
+    return morse.toString().trim();
 }
 
+
+    public String decodificar(String morse) {
+    StringBuilder texto = new StringBuilder();
+    String[] codes = morse.split(" ");
+
+    for (String code : codes) {
+        Character c = reverseMorseMap.get(code);
+        if (c != null) {
+            texto.append(c);
+        }
+    }
+
+    return texto.toString(); 
+}
+
+}
